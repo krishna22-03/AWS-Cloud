64cb00243299c5f2c80c8b74433f2e40bb1af786
krishna22-03 <krishna0322k@gmail.com>
2026-02-15 18:44:27 +0530

    Add files via upload

---
+++ Terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "cidr_block_vpc"{
+    type = string
+}
+
+variable "vpc_name"{
+    type = string
+}
+
+variable "cidr_block_subnet"{
+    type = string
+}
+
+variable "subnet_name"{
+    type = string
+}
+
+variable "igw_name"{
+    type = string
+}
+
+variable "TF_route_table_name"{
+    type = string
+}
